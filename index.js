ee5149f0323a615bfbdd0bbb40edf8896851729e
fd-Martin <fdmosharof@gmail.com>
2024-05-10 13:12:52 +0600

    Make User Admin And Install JWT

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pizauii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,25 +27,62 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection = client.db("bistroDb").collection("menu");
-    const reviewCollection = client.db("bistroDb").collection("reviews");
-    const cartCollection = client.db("bistroDb").collection("carts");
+    // Get the database and collection on which to run the operation
+    const usersCollection = client.db("BistroDB").collection("users");
+    const menuCollection = client.db("BistroDB").collection("menu");
+    const reviewCollection = client.db("BistroDB").collection("reviews");
+    const cartCollection = client.db("BistroDB").collection("carts");
+
+
+    //users related apis
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    //menu related apis
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     })
 
+    //review related apis 
+
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
-      res.send(result);
+      res.send(result)
     })
 
 
     // cart collection apis
+
     app.get('/carts', async (req, res) => {
       const email = req.query.email;
-
       if (!email) {
         res.send([]);
       }
@@ -85,16 +124,3 @@ app.listen(port, () => {
   console.log(`Bistro boss is sitting on port ${port}`);
 })
 
-
-/**
- * --------------------------------
- *      NAMING CONVENTION
- * --------------------------------
- * users : userCollection
- * app.get('/users')
- * app.get('/users/:id')
- * app.post('/users')
- * app.patch('/users/:id')
- * app.put('/users/:id')
- * app.delete('/users/:id')
-*/
